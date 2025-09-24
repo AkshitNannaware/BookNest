b5b367add80742975f37041de215a2429c292913
Akshit <akshitnannaware@gmail.com>
2025-09-24 16:05:53 +0530

    Initial commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
+  build: {
+    chunkSizeWarningLimit: 2000, // increase limit in KB (example: 2000 KB = 2 MB)
+  },
 })
